3a2df9c457e9dba9d0835ad3d109d19d72feb624
yuk6ra <yuki.imamura.all@gmail.com>
2022-11-21 21:49:01 +0900

    Update ERC721S

--- contracts/ERC721SPlans.sol
+++ contracts/ERC721SPlans.sol
@@ -10,7 +10,7 @@ contract ERC721S is ERC721, Ownable {
     
     uint256 public timestamp;
     uint256 public totalSupply;
-    address payable receiveAddr;
+    uint256 public planCount; /// @dev option: 数字
 
     struct Subscription {
         address owner; /// @dev NFTを持っているオーナーのアドレス
@@ -19,6 +19,7 @@ contract ERC721S is ERC721, Ownable {
         uint256 createTime;
         uint256 updateTime;
         uint256 endTime;
+        uint256 planNumber; /// @dev オプション
         bool entry; /// @dev アカウントの登録状態
     }
 
@@ -28,10 +29,9 @@ contract ERC721S is ERC721, Ownable {
         uint256 entryCount;
     }
 
-    Plan public subscPlan;
-
     /// @dev Token Idで管理するほうがわかりやすそう
     mapping(uint256 => Subscription) public subscriptions;
+    mapping(uint256 => Plan) public plans;
 
     constructor(
         uint256 _period,
@@ -40,49 +40,73 @@ contract ERC721S is ERC721, Ownable {
         initSubscriptionPlan(_period, _price);
     }
     
-    function mint() external {
+    function mint(uint256 _planNumber) external payable {
         _safeMint(msg.sender, totalSupply);
-        _createSubscription(totalSupply);
+
+        createSubscription(totalSupply, _planNumber);
 
         totalSupply++;
     }
 
+
     /// @dev 初期化=> Constructorのときに処理を行う
     function initSubscriptionPlan(uint256 _period, uint256 _price) public onlyOwner{
-        receiveAddr = payable(msg.sender);
-        console.log(receiveAddr);
         Plan memory plan = Plan({
             period: _period,
             price: _price,
             entryCount: 0
         });
-        subscPlan = plan;
+        
+        plans[planCount] = plan;
+        planCount++;
+    }
+
+    // @dev プランを追加する
+    function addPlan(
+        uint256 _period,
+        uint256 _price
+    ) public onlyOwner {
+        Plan memory plan = Plan({
+            period: _period,
+            price: _price,
+            entryCount: 0
+        });
+
+        plans[planCount] = plan;
+
+        planCount++; /// @dev プランの数を返す
     }
 
     /// @dev 情報を更新する
     function setPlan(
+        uint256 _planCount,
         uint256 _period,
         uint256 _price
     ) public onlyOwner {
-        // require(plan.period != 0, "No Plan");
-        subscPlan.period = _period;
-        subscPlan.price = _price;
+        require(plans[_planCount].period != 0, "No Plan");
+
+        Plan storage plan = plans[_planCount];
+        plan.period = _period;
+        plan.price = _price;
     }
 
-    function getPlan() public view returns (Plan memory) {
-        require(subscPlan.period != 0, "No Plan");
-        return subscPlan;
+    function deletePlan(uint256 _planNumber) public onlyOwner {
+        require(plans[_planNumber].period != 0, "No Plan");
+        delete plans[_planNumber];
+        planCount--;
+    }
+
+    function getPlan(uint256 _planCount) public view returns (Plan memory) {
+        require(plans[_planCount].period != 0, "No Plan");
+        return plans[_planCount];
     }
 
     /// @dev トークンに紐づくサブスクリプションを作成
-    function _createSubscription(uint256 _tokenId) internal  {
+    function createSubscription(uint256 _tokenId, uint256 _planNumber) public payable  {
         require(!subscriptions[_tokenId].entry, "Already Entry");        
-        require(subscPlan.period != 0, "No Plan");
+        require(plans[_planNumber].period != 0, "No Plan");
         
-        require(msg.value >= subscPlan.price, "Not enought ETH");
-        // require(, "Not enought ETH");
-        // require(msg.value >= subscPlan.price, "Not ")
-        receiveAddr.transfer(msg.value);
+        require(msg.value >= plans[_planNumber].price, "Not enought ETH");
 
         Subscription memory subscription = Subscription({
             owner: msg.sender,
@@ -90,36 +114,42 @@ contract ERC721S is ERC721, Ownable {
             number: 1,
             createTime: block.timestamp,
             updateTime: block.timestamp,
-            endTime: block.timestamp + subscPlan.period,
+            endTime: block.timestamp + plans[_planNumber].period,
+            planNumber: _planNumber,
             entry: true
         });
 
         subscriptions[_tokenId] = subscription;
-        subscPlan.entryCount++;
+        plans[_planNumber].entryCount++;
     }
 
     /// @dev サブスクリプションの更新処理
     function updateSubscription(uint256 _tokenId) public payable {
         require(subscriptions[_tokenId].entry, "No Entry");
         require(
-            msg.value >= subscPlan.price,
+            msg.value >= plans[subscriptions[_tokenId].planNumber].price,
             "Not enought ETH"
         );
         
         Subscription storage subsc = subscriptions[_tokenId];
         subsc.updateTime = block.timestamp;
-        subsc.endTime = block.timestamp + subscPlan.period;
+        subsc.endTime = block.timestamp + plans[subsc.planNumber].period;
         subsc.paidAddress = msg.sender;
 
         subsc.number++;
     }
 
-    /// @dev キャンセル処理→あまり必要がない
-    function cancelSubscription(uint256 tokenId) public {
-        Subscription storage subsc = subscriptions[tokenId];
-        subsc.entry = false;
-    }
+    /// @dev 更新のオプション処理
+    function updatePlan(uint256 _tokenId, uint256 _planNumber) public {        
+        require(subscriptions[_tokenId].entry, "No Entry");
+        require(plans[_planNumber].period != 0, "No Plan");
 
+        Subscription storage subsc = subscriptions[_tokenId];        
+        subsc.planNumber = _planNumber;
+
+        updateSubscription(_tokenId); /// @dev サブスクリプションの処理も行う
+    }
+    
     /// @dev サブスクの登録ができているかどうか
     function getEntry(uint256 tokenId) public view returns (bool) {
         return subscriptions[tokenId].entry;
@@ -143,4 +173,6 @@ contract ERC721S is ERC721, Ownable {
     // function getBool() external view returns (bool) {
     //     return timestamp + 30 seconds > block.timestamp;
     // }
+
+
 }
